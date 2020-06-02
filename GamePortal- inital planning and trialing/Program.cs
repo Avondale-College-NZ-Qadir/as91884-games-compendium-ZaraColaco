cf95e05521b7b9a33d314804e424b2a1234fb9eb
Zara Colaco <ac106503@avcol.school.nz>
2020-06-02 12:25:59 +1200

    Add project files.

---
+++ GamePortal- inital planning and trialing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Timers;
+
+namespace GamePortal__inital_planning_and_trialing
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            // Create a timer with a one minute interval.
+            Timer aTimer = new System.Timers.Timer(60000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            Console.ReadKey();
+
+            }
+        public static void OnTimedEvent(Object source, ElapsedEventArgs e)
+            {
+            
+            }
+
+
+        }
+    }
