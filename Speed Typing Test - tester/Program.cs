56f8743673534783ca2968ff1179d1e83c47cdcc
Zara Colaco <ac106503@avcol.school.nz>
2020-06-15 13:37:23 +1200

    Commit 1

--- Speed Typing Test - tester/Program.cs
+++ Speed Typing Test - tester/Program.cs
@@ -1,21 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Speed_Typing_Test___tester
     {
     class Program
         {
-        private static List<string> PHRASES = new List<string>();
+        private static List<string> TextList = new List<string>();
+        private static string UserText = " ";
+
+        //string FileName = " ";
         static void Main(string[] args)
             {
-
+            int randNum = RandomNumber(1, 5);
+            string FileName = "story" + randNum.ToString() +".txt";
             Console.WriteLine("Insturctions: Copy the phrase! Case and punctuation matter. Do try your best :)");
-            Console.WriteLine("Press any enter to start");
+            string text = System.IO.File.ReadAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\" + FileName);
+            //C:\Users\ac106503\Source\Repos\as91884-games-compendium-ZaraColaco
+            System.Console.WriteLine(text);
+            Console.WriteLine("Press enter to start");
             Console.ReadLine();
-            string text = System.IO.File.ReadAllText(@"C:C:\Users\ac106503\Source\Repos\as91884-games-compendium-ZaraColaco\story4");
-            System.Console.WriteLine( text);
-           
+            UserText = Console.ReadLine();
+            System.IO.File.WriteAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\userstext.txt", UserText);
 
-                }
+
+
+
+            Console.ReadKey();
+
+
+
+
             }
-    }   }
+           public static int RandomNumber(int min, int max) 
+            {
+            Random random = new Random();
+            return random.Next(min, max);
+            }
+          
+        }
+    }
+     
+      
+   
