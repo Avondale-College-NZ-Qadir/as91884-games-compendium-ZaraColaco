789a4e648cd31e786b66b482794bb01cbdda04b5
Zara Colaco <ac106503@avcol.school.nz>
2020-06-16 11:35:32 +1200

    commit 3

--- Speed Typing Test - tester/Program.cs
+++ Speed Typing Test - tester/Program.cs
@@ -15,16 +15,20 @@ namespace Speed_Typing_Test___tester
             {
             int randNum = RandomNumber(1, 5);
             string FileName = "story" + randNum.ToString() +".txt";
-            Console.WriteLine("Insturctions: Copy the phrase! Case and punctuation matter. Do try your best :)");
+            Console.WriteLine("Insturctions: Copy the phrase! Case and punctuation matter. Do try your best :)\n");
             string text = System.IO.File.ReadAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\" + FileName);
             //C:\Users\ac106503\Source\Repos\as91884-games-compendium-ZaraColaco
             System.Console.WriteLine(text);
-            Console.WriteLine("Press enter to start");
+            Console.WriteLine("\nPress enter to start");
             Console.ReadLine();
+            Console.WriteLine("your 60 seconds starts NOW!!!\n");
             UserText = Console.ReadLine();
             System.IO.File.WriteAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\userstext.txt", UserText);
+           
+           // System.IO.File.WriteAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\userstext.txt", String.Empty);
+           //For Later
 
-
+            //compare, score wpm and accuracy % method, carryon loop, stopping breaks- boundaries., Testing -----> Add to menu
 
 
             Console.ReadKey();
