48c39b4a5285b2a812966f1eea2b5848ec1e26b3
Zara Colaco <ac106503@avcol.school.nz>
2020-06-15 13:57:54 +1200

    commit 2

--- Speed Typing Test - tester/Program.cs
+++ Speed Typing Test - tester/Program.cs
@@ -16,8 +16,8 @@ namespace Speed_Typing_Test___tester
             int randNum = RandomNumber(1, 5);
             string FileName = "story" + randNum.ToString() +".txt";
             Console.WriteLine("Insturctions: Copy the phrase! Case and punctuation matter. Do try your best :)");
-            string text = System.IO.File.ReadAllText(@"C:\Users\colac\Source\Repos\as91884-games-compendium-ZaraColaco2\" + FileName);
-            //C:\Users\ac106503\Source\Repos\as91884-games-compendium-ZaraColaco
+            string text = System.IO.File.ReadAllText(@"C:\Users\ac106503\Source\Repos\as91884-games-compendium-ZaraColaco" + FileName);
+            //..\Repos\as91884-games-compendium-ZaraColaco
             System.Console.WriteLine(text);
             Console.WriteLine("Press enter to start");
             Console.ReadLine();
