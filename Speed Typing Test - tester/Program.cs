80ea8151067823a35ce91172537db99a4a1cfe2e
Zara Colaco <ac106503@avcol.school.nz>
2020-06-18 10:10:17 +1200

    commit timer part 1

--- Speed Typing Test - tester/Program.cs
+++ Speed Typing Test - tester/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Timers;
 using System.IO.Enumeration;
 using System.Security.Cryptography.X509Certificates;
 
@@ -10,6 +11,7 @@ namespace Speed_Typing_Test___tester
         {
         private static List<string> TextList = new List<string>();
         private static string UserText = " ";
+        private static Timer MinuteTimer;
 
         //string FileName = " ";
         static void Main(string[] args)
@@ -20,16 +22,28 @@ namespace Speed_Typing_Test___tester
             string pathOfFile = Path.GetFullPath(FileName);
             pathOfFile = pathOfFile.Replace(@"\","/");
             pathOfFile = pathOfFile.Replace(@"/bin/Debug/netcoreapp3.1/" + FileName , "/" + FileName);
-            Console.WriteLine(pathOfFile);
             string text = System.IO.File.ReadAllText(pathOfFile);
             System.Console.WriteLine(text);
             Console.WriteLine("\nPress enter to start");
-            Console.ReadLine();
+            Console.ReadLine(); 
             Console.WriteLine("your 60 seconds starts NOW!!!\n");
-            UserText = Console.ReadLine();
-            System.IO.File.WriteAllText(pathOfFile + "/userstext.txt", UserText);
-           
-           System.IO.File.WriteAllText(pathOfFile + "/userstext.txt", String.Empty);
+            MinuteTimer = new System.Timers.Timer(60000);
+            MinuteTimer.Start();
+            Console.WriteLine(MinuteTimer);
+
+            //while (MinuteTimer != null)
+            //    {
+            //    UserText = Console.ReadLine();
+            //    pathOfFile = pathOfFile.Replace(@FileName, "userstext.txt");
+            //    System.IO.File.WriteAllText(pathOfFile, UserText);
+            //    }
+            //while(MinuteTimer == null)
+            //    {
+            //    Console.WriteLine("TIME'S UP!!");
+            //    }
+            
+            //System.IO.File.WriteAllText(pathOfFile , String.Empty);
+ 
            //For Later
 
             //compare, score wpm and accuracy % method, carryon loop, stopping breaks- boundaries., Testing -----> Add to menu
---
+++ Speed Typing Test - tester/TimerState.cs
@@ -0,0 +1,8 @@
+﻿namespace Speed_Typing_Test___tester
+    {
+    internal class TimerState
+        {
+        internal int counter;
+        internal object tmr;
+        }
+    }
