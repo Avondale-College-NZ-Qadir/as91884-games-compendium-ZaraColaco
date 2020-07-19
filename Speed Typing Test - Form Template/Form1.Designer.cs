688ac86fac0c2e24ba6e98c65f4311bef4ab5f01
Zara Colaco <ac106503@avcol.school.nz>
2020-07-19 15:41:12 +1200

    Add project files.

---
+++ Speed Typing Test - Form Template/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Speed_Typing_Test___Form_Template
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.components = new System.ComponentModel.Container();
+            this.TBUserInput = new System.Windows.Forms.RichTextBox();
+            this.BTStart = new System.Windows.Forms.Button();
+            this.TBStory = new System.Windows.Forms.TextBox();
+            this.LBdisplay = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BTScore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TBUserInput
+            // 
+            this.TBUserInput.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBUserInput.ForeColor = System.Drawing.Color.Olive;
+            this.TBUserInput.Location = new System.Drawing.Point(195, 677);
+            this.TBUserInput.Name = "TBUserInput";
+            this.TBUserInput.Size = new System.Drawing.Size(1086, 440);
+            this.TBUserInput.TabIndex = 4;
+            this.TBUserInput.Text = "";
+            this.TBUserInput.Visible = false;
+            this.TBUserInput.TextChanged += new System.EventHandler(this.TBUserInput_TextChanged);
+            // 
+            // BTStart
+            // 
+            this.BTStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTStart.ForeColor = System.Drawing.Color.Red;
+            this.BTStart.Location = new System.Drawing.Point(595, 565);
+            this.BTStart.Name = "BTStart";
+            this.BTStart.Size = new System.Drawing.Size(310, 57);
+            this.BTStart.TabIndex = 5;
+            this.BTStart.Text = "Click to Start";
+            this.BTStart.UseVisualStyleBackColor = true;
+            this.BTStart.Click += new System.EventHandler(this.BTStart_Click);
+            // 
+            // TBStory
+            // 
+            this.TBStory.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBStory.Location = new System.Drawing.Point(195, 82);
+            this.TBStory.Multiline = true;
+            this.TBStory.Name = "TBStory";
+            this.TBStory.ReadOnly = true;
+            this.TBStory.Size = new System.Drawing.Size(1086, 467);
+            this.TBStory.TabIndex = 6;
+            // 
+            // LBdisplay
+            // 
+            this.LBdisplay.AutoSize = true;
+            this.LBdisplay.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBdisplay.ForeColor = System.Drawing.Color.DarkCyan;
+            this.LBdisplay.Location = new System.Drawing.Point(596, 625);
+            this.LBdisplay.Name = "LBdisplay";
+            this.LBdisplay.Size = new System.Drawing.Size(319, 49);
+            this.LBdisplay.TabIndex = 7;
+            this.LBdisplay.Text = "                                 ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(176, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1095, 49);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Instructions: Copy the text! Case and punctuation matter. Do try your best ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BTScore
+            // 
+            this.BTScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTScore.Location = new System.Drawing.Point(595, 1123);
+            this.BTScore.Name = "BTScore";
+            this.BTScore.Size = new System.Drawing.Size(310, 61);
+            this.BTScore.TabIndex = 9;
+            this.BTScore.Text = "Click for score";
+            this.BTScore.UseVisualStyleBackColor = true;
+            this.BTScore.Visible = false;
+            this.BTScore.Click += new System.EventHandler(this.BTScore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 1269);
+            this.Controls.Add(this.BTScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LBdisplay);
+            this.Controls.Add(this.TBStory);
+            this.Controls.Add(this.BTStart);
+            this.Controls.Add(this.TBUserInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox TBUserInput;
+        private System.Windows.Forms.Button BTStart;
+        private System.Windows.Forms.TextBox TBStory;
+        private System.Windows.Forms.Label LBdisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BTScore;
+        }
+    }
+
---
+++ Speed Typing Test - Form Template/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Drawing;
+using System.Linq;
+//using System.Timers;
+//using Timer = System.Timers.Timer;
+
+namespace Speed_Typing_Test___Form_Template
+    {
+    public partial class Form1 : Form
+        {
+        //public static Timer timer = new Timer(30000);
+        //public static bool stopTimer = false;
+        public Form1()
+            {
+            InitializeComponent();
+            }
+        string UserFile = "";
+        string StoryFile = "";
+        
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            LBdisplay.Text = "";
+            TBUserInput.Visible = false;
+            int randNum = RandomNumber(1, 5);
+            string FileName = "story" + randNum.ToString() + ".txt";
+            string pathOfFile = Path.GetFullPath(FileName);
+            pathOfFile = pathOfFile.Replace(@"\", "/");
+            StoryFile = pathOfFile.Replace(@"/bin/Debug/" + FileName, "/" + FileName);
+            TBStory.Text = File.ReadAllText(StoryFile);
+            UserFile = pathOfFile.Replace(@"/bin/Debug/" + FileName, "/userstext.txt");
+            
+
+
+  
+
+
+            }
+        private static int RandomNumber(int min, int max)
+            {
+            Random random = new Random();
+            return random.Next(min, max);
+            }
+
+
+        private void TBUserInput_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void BTStart_Click(object sender, EventArgs e)
+            {
+            BTStart.Visible = false;
+            LBdisplay.Text = "Your 60 seconds starts NOW!!!";
+            //DateTime time = DateTime.Now;
+            //DateTime timeOver = time.AddSeconds(30);
+            TBUserInput.Visible = true;
+            TBUserInput.ReadOnly = false;
+            Timer timer1 = new Timer
+                {
+                Interval = 20000
+                };
+            timer1.Enabled = true;
+            timer1.Tick += new System.EventHandler(OnTimerEvent);
+            TBUserInput.Focus();
+
+
+
+
+            //users cursor position set in TBUserInput
+            }
+        public void OnTimerEvent(object sender, EventArgs e)
+            {
+            TBUserInput.ReadOnly = true;
+            LBdisplay.Text = "Time's up!!!";
+            TextWriter txt = new StreamWriter(UserFile);
+            txt.Write(TBUserInput.Text);
+            txt.Close();
+            BTScore.Visible = true;
+
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+            {
+
+            }
+
+        private void BTScore_Click(object sender, EventArgs e)
+            {
+            BTScore.Visible = false;
+            string[] test1 = TBStory.Text.Split(' ');
+            string[] test2 = TBUserInput.Text.Split(' ');
+            List<string> storyList = test1.ToList();
+            List<string> responseList = test2.ToList();
+            //List<string> resultsList = test2.ToList();
+            //int i;
+            //for (i in range(len(storyList)))
+            //    {
+            //    if(storyList[i]>responseList[i])
+            //        {
+            //        resultsList.Append(1);
+            //        }
+            //    else
+            //        {
+            //        resultsList.Append(0);
+            //        }
+
+            //    }
+            var firstNotSecond = storyList.Except(responseList).ToList();
+            //int ctr = 0;
+            //int y = 0;
+            //for (int i = 0; i < storyList.Count && i < responseList.Count; i++)
+            //    {
+            //    if (resultsList[i] == storyList[i])
+
+            //        {
+            //        ctr++;
+            //        }
+            //    else
+            //        {
+            //        if (resultsList[i] == storyList[i + 1])
+            //            {
+
+            //            }
+            //        }
+            //    }
+            }
+        }
+    }
